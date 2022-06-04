2037d6294b14a59a62f2765ef3b007a23e3b08bf
Doseeey <jakubdebosz.it@gmail.com>
2022-06-04 14:20:24 +0200

    added word generation

--- wordle.py
+++ wordle.py
@@ -1,14 +1,24 @@
 import pygame
-from consonants import Consonants
+from constants import Constants as C
+from random import choice
 
 pygame.init()
 pygame.display.set_caption("Wordle game clone")
-SCREEN = pygame.display.set_mode((Consonants.HEIGHT, Consonants.WIDTH))
+WIDTH, HEIGHT = 800, 800
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 30
 
+def get_word():
+    wordlist = [word.replace("\n", "") for word in list(open("wordlist.txt"))]
+    word = choice(wordlist)
+    assert(len(word) == C.LETTER_LENGTH)
+    assert(word.islower())
+
 def main(): 
+    clock = pygame.time.Clock()
+    guess_word = get_word()
     while True:
-        pass
+        
 
 if __name__ == "__main__":
     main()
